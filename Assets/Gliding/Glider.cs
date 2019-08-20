e5066a74eac882dfc66aa71a7d83c987590246bb
unknown <erw331@nyu.edu>
2019-08-20 21:35:57 +0100

    New take on glider start

--- Assets/Gliding/Glider.cs
+++ Assets/Gliding/Glider.cs
@@ -2,14 +2,6 @@
 
 public class Glider : MonoBehaviour
 {
-    /*
-    public float mass;
-    public float airForcesFactor; //surface area of wings * density of air * 0.5
-    public float liftCoefficient;
-    public float dragCoefficient;
-    public float gravityAcceleation;
-    */
-
     //Insepctor Variables
     public float timeScale;
     public float maxGlideRatio;
@@ -17,13 +9,14 @@ public class Glider : MonoBehaviour
 
     public float liftToLateralRatio;
     public float lateralDrag;
+    public float vScale;
+    public float thetaScale;
 
     //Variables for calculation
     private float pitchAngle; //in radians and anti-clockwise
     private float rollAngle;
 
     //Variables, refreshed frame-to-frame
-    private float R;
     private float vy;
     private float vx;
     private float t;
@@ -42,9 +35,10 @@ public class Glider : MonoBehaviour
 
     public void Pitch(float angle)
     {
-        pitchAngle += angle * t;
+        transform.Rotate(transform.right, angle);
     }
 
+    //TODO: lerp
     public void Level()
     {
         rollAngle -= rollAngle;
@@ -68,8 +62,6 @@ public class Glider : MonoBehaviour
 
     private void UpdateXPhysics()
     {
-        R = maxGlideRatio * Mathf.Cos(rollAngle);
-
         float acc = (liftToLateralRatio * maxGlideRatio * Mathf.Sin(rollAngle));
         vx += acc * t;
 
@@ -79,9 +71,9 @@ public class Glider : MonoBehaviour
 
     private void UpdateYZPhysics()
     {
-        float acc = -Mathf.Sin(pitchAngle) - (vy * vy / R);
+        float acc = -Mathf.Sin(pitchAngle) - (vy * vy / maxGlideRatio);
 
-        float angAcc = -Mathf.Cos(pitchAngle) / vy + vy;
+        float angAcc = (-Mathf.Cos(pitchAngle) / vy + vy);
 
         vy += acc * t;
         pitchAngle += angAcc * t;
@@ -91,8 +83,8 @@ public class Glider : MonoBehaviour
     private void UpdateTransform()
     {
         float x = vx * t;
-        float y = vy * Mathf.Sin(pitchAngle) * t;
-        float z = vy * Mathf.Cos(pitchAngle) * t;
+        float y = vy * Mathf.Sin(pitchAngle) * t * vScale;
+        float z = vy * Mathf.Cos(pitchAngle) * t * vScale;
 
         Velocity = new Vector3(x, y, z);
 
@@ -107,55 +99,6 @@ public class Glider : MonoBehaviour
         var headingVec = new Vector3(transform.position.x, 0f, transform.position.z).normalized;
         Heading = -Mathf.Rad2Deg * MathUtility.IntoRange(Mathf.Acos(Vector3.Dot(Vector3.forward, headingVec)), -180f, 180f);
     }
-
-    /*
-    private void FixedUpdate()
-    {
-        float V = currentVelocity.z;
-        float t = Time.fixedDeltaTime;
-
-        forwardAcceleration = ((-mass * gravityAcceleation * Mathf.Sin(angleOfAttack)) - Drag(V)) / mass;
-
-        angularAccRadians = ((-mass * gravityAcceleation * Mathf.Cos(angleOfAttack)) + Lift(V, angleOfAttack * Mathf.Rad2Deg)) / (mass * V);
-
-        currentAcceleration = forwardAcceleration * transform.forward;
-        currentVelocity += currentAcceleration * t;
-        glideRatio = currentVelocity.z / currentVelocity.y;
-
-        transform.position += currentVelocity * t;
-
-        this.angleOfAttack = Normalise(angleOfAttack + (angularAccRadians * Time.fixedDeltaTime), 0f, 360f);
-
-        transform.rotation = Quaternion.Euler(new Vector3(TransformToCalcAngle(this.angleOfAttack), 0f, 0f));
-    }
-
-    private float Lift(float forwardVelocity, float degAngleOfAttack)
-    {
-        return (Mathf.Max(0, LiftCoefficient(degAngleOfAttack))) * airForcesFactor * forwardVelocity * forwardVelocity;
-    }
-
-    private float LiftCoefficient(float degAngleOfAttack)
-    {
-        return 0.6394558f + 0.1117347f * degAngleOfAttack - 0.003231293f * degAngleOfAttack * degAngleOfAttack;
-    }
-
-    private float Drag(float forwardVelocity)
-    {
-        return dragCoefficient * airForcesFactor * forwardVelocity * forwardVelocity;
-    }
-
-    private float GetTransformAngle()
-    {
-        Vector3 horizontal = Vector3.ProjectOnPlane(transform.forward, Vector3.up);
-
-        return Vector3.SignedAngle(transform.forward, horizontal, -transform.right);
-    }
-
-    private float TransformToCalcAngle(float transformAngle)
-    {
-        return -Normalise(Mathf.Deg2Rad * transformAngle, 0f, Mathf.PI * 2f);
-    }
-    */
 
     private float CalcToTransformAngle(float calcAngle)
     {
